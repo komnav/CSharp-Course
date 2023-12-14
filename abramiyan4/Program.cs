3972d82d32d74e104f0173a10ef50ac86b7e0cb5
Navruz <navruzkomilov503@gmail.com>
2023-12-14 14:10:24 +0500

    abramiyan4

---
+++ abramiyan4/Program.cs
@@ -0,0 +1,82 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+/*Console.WriteLine("x1?");
+var x1=double.Parse(Console.ReadLine());
+Console.WriteLine("y1?");
+var y1 = double.Parse(Console.ReadLine());
+Console.WriteLine("x2?");
+var x2 = double.Parse(Console.ReadLine());
+Console.WriteLine("y2?");
+var y2 = double.Parse(Console.ReadLine());
+double A=Math.Sqrt(Math.Pow(x2-x1,2)+Math.Pow(y2-y1,2));
+Console.WriteLine(A);*/
+
+/*Console.WriteLine("x1?");
+var x1=double.Parse(Console.ReadLine());
+Console.WriteLine("y1?");
+var y1 = double.Parse(Console.ReadLine());
+Console.WriteLine("x2?");
+var x2 = double.Parse(Console.ReadLine());
+Console.WriteLine("y2?");
+var y2 = double.Parse(Console.ReadLine());
+Console.WriteLine("x3?");
+var x3 = double.Parse(Console.ReadLine());
+Console.WriteLine("y3?");
+var y3 = double.Parse(Console.ReadLine());
+double AB=Math.Sqrt(Math.Pow(x2-x1, 2)+Math.Pow(y2-y1,2));
+double BC = Math.Sqrt(Math.Pow(x3 - x2, 2) + Math.Pow(y3 - y2, 2));
+double AC = Math.Sqrt(Math.Pow(x3 - x1, 2) + Math.Pow(y3 - y1, 2));
+double perimetr=AB+BC+AC;
+double p = perimetr / 2;
+double S = Math.Sqrt(p * (p - AB) * (p - BC) * (p - AC));
+Console.WriteLine(p);
+Console.WriteLine(S);*/
+
+/*Console.WriteLine("A?");
+var A=double.Parse(Console.ReadLine());
+Console.WriteLine("B?");
+var B = double.Parse(Console.ReadLine());
+double a = B;
+double b = A;
+Console.WriteLine(a);
+Console.WriteLine(b);*/
+
+/*Console.WriteLine("A?");
+var A = double.Parse(Console.ReadLine());
+Console.WriteLine("C?");
+var C = double.Parse(Console.ReadLine());
+Console.WriteLine("B?");
+var B = double.Parse(Console.ReadLine());
+double a = B;
+double b = C;
+double c = A;
+Console.WriteLine(a);
+Console.WriteLine(b);
+Console.WriteLine(c);*/
+
+/*Console.WriteLine("A?");
+var A = double.Parse(Console.ReadLine());
+Console.WriteLine("C?");
+var C = double.Parse(Console.ReadLine());
+Console.WriteLine("B?");
+var B = double.Parse(Console.ReadLine());
+double a = C;
+double b = A;
+double c = B;
+Console.WriteLine(a);
+Console.WriteLine(b);
+Console.WriteLine(c);*/
+
+Console.WriteLine("X?");
+var x=double.Parse(Console.ReadLine());
+double y=3*Math.Pow(x,6)-6*x*x/7;
+Console.WriteLine(y);
+
+
+
+
+
+
+
+
