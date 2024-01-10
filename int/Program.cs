5ed3c35c5e13ae1f7e0fd8e48dcf8e6d641bfdc0
Navruz <navruzkomilov503@gmail.com>
2024-01-10 23:38:13 +0500

    integer

---
+++ int/Program.cs
@@ -0,0 +1,62 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("L?");//1
+//var L=int.Parse(Console.ReadLine());
+//double SM = L * 100;
+//Console.WriteLine($"{L}metr={SM}santimetr");
+
+//Console.WriteLine("M?");//2
+//var M = int.Parse(Console.ReadLine());
+//double KG = M * 1000;
+//Console.WriteLine($"{M}tonna={KG}kelogram");
+
+//Console.WriteLine("B?");//3
+//var kb = int.Parse(Console.ReadLine());
+//double B = kb * 1024;
+//Console.WriteLine($"{kb}kelobait={B}baita");
+
+//Console.WriteLine("a");//4
+//var a = int.Parse(Console.ReadLine());
+//Console.WriteLine("b");
+//var b = int.Parse(Console.ReadLine());
+//double t = a / b;
+//Console.WriteLine(t);
+
+//Console.WriteLine("a");//5
+//var a = int.Parse(Console.ReadLine());
+//Console.WriteLine("b");
+//var b = int.Parse(Console.ReadLine());
+//double t = a % b;
+//Console.WriteLine(t);
+
+//Console.WriteLine("a");//6
+//var a = int.Parse(Console.ReadLine());
+//double t = a /10;
+//double t1 = a % 10;
+//Console.WriteLine(t);
+//Console.WriteLine(t1);
+
+//Console.WriteLine("a");//7
+//var a = int.Parse(Console.ReadLine());
+//double t = a / 10;
+//double t1 = a % 10;
+//double s = t + t1;
+//double z = t * t1;
+//Console.WriteLine(s);
+//Console.WriteLine(z);
+
+//Console.WriteLine("a");//8
+//var a = int.Parse(Console.ReadLine());
+//double t = a / 10;
+//double t1 = a % 10;
+//Console.WriteLine($"{t1}{t}");
+
+//Console.WriteLine("a");//9
+//var a = int.Parse(Console.ReadLine());
+//double A = a / 100;
+//Console.WriteLine(A);
+
+Console.WriteLine("a");//10
+var a = int.Parse(Console.ReadLine());
+double A = a % 10;
+double s = a / 10 % 10;
+Console.WriteLine($"{A}    {s}");
