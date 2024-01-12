5e6026d21cbff6ff1919cdb186222aa3edb8b05c
Navruz <navruzkomilov503@gmail.com>
2024-01-12 14:13:35 +0500

    int 11/20

---
+++ int2/Program.cs
@@ -0,0 +1,72 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("a?");11
+//var a = int.Parse(Console.ReadLine());
+//double z = a / 100;
+//double z1 = a / 10 % 10;
+//double z2 = a % 10;
+//double summa = z + z1 + z2;
+//double min = z - z1 - z2;
+//Console.WriteLine($"summa={summa}  tarh={min}");
+
+//Console.WriteLine("a?");//12
+//var a = int.Parse(Console.ReadLine());
+//double z = a / 100;
+//double z1 = a / 10 % 10;
+//double z2 = a % 10;
+//Console.WriteLine($"{z2}{z1}{z}");
+
+//Console.WriteLine("a?");//13
+//var a = int.Parse(Console.ReadLine());
+//double z = a / 100;
+//double z1 = a / 10 % 10;
+//double z2 = a % 10;
+//Console.WriteLine($"{z1}{z2}{z}");
+
+//Console.WriteLine("a?");//14
+//var a = int.Parse(Console.ReadLine());
+//double z = a / 100;
+//double z1 = a / 10 % 10;
+//double z2 = a % 10;
+//Console.WriteLine($"{z2}{z}{z1}");
+
+
+//Console.WriteLine("a?");//15
+//var a = int.Parse(Console.ReadLine());
+//double z = a / 100;
+//double z1 = a / 10 % 10;
+//double z2 = a % 10;
+//Console.WriteLine($"{z1}{z}{z2}");
+
+//Console.WriteLine("a?");//16
+//var a = int.Parse(Console.ReadLine());
+//double z = a / 100;
+//double z1 = a / 10 % 10;
+//double z2 = a % 10;
+//Console.WriteLine($"{z}{z2}{z1}");
+
+
+//Console.WriteLine("a?");//17
+//var a=int.Parse(Console.ReadLine());
+//double s = a / 1000%10;
+//Console.WriteLine($"{s}00");
+
+
+//Console.WriteLine("a?");//18
+//var a = int.Parse(Console.ReadLine());
+//double s = a / 1000 % 10;
+//Console.WriteLine($"{s}000");
+
+
+//Console.WriteLine("a?");//19
+//var a=int.Parse(Console.ReadLine());
+//double s = a / 60;
+////double s1 = s + 60;
+//Console.WriteLine(s);
+
+Console.WriteLine("a?");//20
+var a = int.Parse(Console.ReadLine());
+double s = a /3600 ;
+Console.WriteLine(s);
+
+
+
